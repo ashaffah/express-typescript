b984e23ace13a1b1a9a94e301ccc68b5f5bd5a15
Ashaffah <wagamama.morph@gmail.com>
2023-09-11 15:28:46 +0700

    update schema model

---
+++ prisma/migrations/20230911080938_schema_update/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Post` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Post" ADD COLUMN     "deletedAt" TIMESTAMP(3),
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
