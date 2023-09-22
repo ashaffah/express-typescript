0ee6b25939eca73138421cbe162f1e68abee1221
Ashaffah <wagamama.morph@gmail.com>
2023-09-22 13:45:48 +0700

    fix method

--- routes/exampleRoutes.ts
+++ routes/exampleRoutes.ts
@@ -20,7 +20,7 @@ router.get("/post/:id", exampleGetById); // Get Dada By Id
 router.put("/post/:id", exampleUpdate); // Update Data By Id
 router.delete("/post/perma_delete/:id", exampleDelete); // Hard Delete Data
 router.delete("/post/delete/:id", exampleSoftDelete); // Soft Delete Data
-router.post("/post/restore_all", exampleRestoreAllSoftDelete); // Restore All Data
+router.patch("/post/restore_all", exampleRestoreAllSoftDelete); // Restore All Data
 router.put("/post/restore/:id", exampleRestoreSoftDelete); // Restore Data By Id
 router.get("/deleted", exampleGetAllSoftDelete); // Get All Soft Deleted
 router.get("/deleted/:id", exampleGetDeletedById); // Get Soft Deleted By Id
